1ac9fe5e943228ab887ebb3f7aa08819bdec124b
lemos <xfe1235@gmail.com>
2023-05-21 14:55:51 +0800

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Alfred Jobs on 2023/5/21.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
